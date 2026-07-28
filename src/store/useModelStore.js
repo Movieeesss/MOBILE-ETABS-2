97d53c74cc9df947177cfb008f2daaab2200cfc4
Movieeesss <114383509+Movieeesss@users.noreply.github.com>
2026-07-29 00:12:20 +0530

    Update useModelStore.js

--- src/store/useModelStore.js
+++ src/store/useModelStore.js
@@ -1,19 +1,11 @@
 import { create } from 'zustand';
 
 export const useModelStore = create((set) => ({
-  loadCase: 'DEAD',
-  resultType: 'undeformed',
-  showColumns: true,
-  showBeams: true,
-  showSlabs: true,
-  deformationScale: 50,
-  diagramScale: 0.05,
+  // ETABS Style Grid Spacings (Default)
+  xSpacing: [4.0, 3.5, 5.0],
+  ySpacing: [4.0, 4.0],
   
-  setLoadCase: (loadCase) => set({ loadCase }),
-  setResultType: (resultType) => set({ resultType }),
-  toggleColumns: () => set((state) => ({ showColumns: !state.showColumns })),
-  toggleBeams: () => set((state) => ({ showBeams: !state.showBeams })),
-  toggleSlabs: () => set((state) => ({ showSlabs: !state.showSlabs })),
-  setDeformationScale: (scale) => set({ deformationScale: scale }),
-  setDiagramScale: (scale) => set({ diagramScale: scale }),
+  // Actions to update grids
+  setXSpacing: (newSpacing) => set({ xSpacing: newSpacing }),
+  setYSpacing: (newSpacing) => set({ ySpacing: newSpacing }),
 }));
